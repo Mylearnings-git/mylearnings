1ae92b1a3a486ca27b21abdf88287becfb1dcb04
Mylearnings-git <62610302+Mylearnings-git@users.noreply.github.com>
2020-05-18 15:58:40 +0530

    Update Dockerbuild.groovy

--- vars/Dockerbuild.groovy
+++ vars/Dockerbuild.groovy
@@ -1,9 +1,3 @@
 def call() {
-    
-    pipeline {
-        agent any
-        stages {
-            stage('Build maven') {
-                steps {
-    sh "mvn clean install"
-                }}}}}
+      sh "mvn clean install"
+                }

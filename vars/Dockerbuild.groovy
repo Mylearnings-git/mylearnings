8d87cb99cfa69c83dc6a718a83bdd94f21e840bf
Mylearnings-git <62610302+Mylearnings-git@users.noreply.github.com>
2020-05-18 15:56:47 +0530

    Update Dockerbuild.groovy

--- vars/Dockerbuild.groovy
+++ vars/Dockerbuild.groovy
@@ -1,18 +1,9 @@
-def call(Map project, Map hubUser) {
+def call() {
     
     pipeline {
         agent any
         stages {
-            stage('Build docker git') {
+            stage('Build maven') {
                 steps {
-    sh "docker image build -t hubUser.user/project.name:beta-${env.BRANCH_NAME}-${env.BUILD_NUMBER} ."
-    withCredentials([usernamePassword(
-            credentialsId: "Docker",
-            usernameVariable: "Username",
-            passwordVariable: "Password"
-    )]) {
-        sh "docker login -u '$Username' -p '$Password'"
-    }
-    sh "docker image push ${hubUser}/${project}:beta-${env.BRANCH_NAME}-${env.BUILD_NUMBER}"
-}
-            }}}}
+    sh "mvn clean install"
+                }}}}}

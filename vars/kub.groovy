e8478b6448f463cd67011222b8232af0276ff3a4
Mylearnings-git <62610302+Mylearnings-git@users.noreply.github.com>
2020-05-19 15:24:41 +0530

    Update kub.groovy

--- vars/kub.groovy
+++ vars/kub.groovy
@@ -6,7 +6,7 @@
  secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) 
  {
               echo "Login Successfull"
-              sh 'aws eks --region ${region}  update-kubeconfig --name ${cluster}'
+              sh "aws eks --region '${region}'  update-kubeconfig --name '${cluster}'"
               sh 'sed -i s/"Build_Number"/"$BUILD_NUMBER"/g deployment.yml'
               sh 'kubectl apply -f deployment.yml'
               sh 'kubectl apply -f service.yml'

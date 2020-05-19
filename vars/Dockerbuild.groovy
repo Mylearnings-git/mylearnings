fa190f88b9a507f68871e0f0292a41ba0bc357d1
Mylearnings-git <62610302+Mylearnings-git@users.noreply.github.com>
2020-05-19 13:55:06 +0530

    Create Dockerbuild.groovy

---
+++ vars/Dockerbuild.groovy
@@ -0,0 +1,11 @@
+def call(String project, String hubUser) {
+    sh "docker image build -t ${hubUser}/${project}:beta-${env.BRANCH_NAME}-${env.BUILD_NUMBER} ."
+    withCredentials([usernamePassword(
+            credentialsId: "Docker",
+            usernameVariable: "Username",
+            passwordVariable: "Password"
+    )]) {
+        sh "docker login -u '$Username' -p '$Password'"
+    }
+    sh "docker image push ${hubUser}/${project}:beta-${env.BRANCH_NAME}-${env.BUILD_NUMBER}"
+}

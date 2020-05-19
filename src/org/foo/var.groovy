7a23894f66336bd6c4c3875571d3ff2f73d2bd20
Mylearnings-git <62610302+Mylearnings-git@users.noreply.github.com>
2020-05-19 16:35:39 +0530

    Update var.groovy

--- src/org/foo/var.groovy
+++ src/org/foo/var.groovy
@@ -1,5 +1,5 @@
 package org.foo
 
 class GlobalVars {
-   static String url = "https://github.com/Mylearnings-git/mylearnings.git"
+   static def String url = "https://github.com/Mylearnings-git/mylearnings.git"
 }

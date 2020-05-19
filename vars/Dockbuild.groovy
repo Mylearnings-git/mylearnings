b9c9be9f9859fbeac2947551dd8e9942ea5ef427
Mylearnings-git <62610302+Mylearnings-git@users.noreply.github.com>
2020-05-19 14:03:15 +0530

    Update Dockbuild.groovy

--- vars/Dockbuild.groovy
+++ vars/Dockbuild.groovy
@@ -1,5 +1,5 @@
 def call(String project, String hubUser) {
-    sh "docker image build -t ${hubUser}/${project}:beta-${env.BRANCH_NAME}-${env.BUILD_NUMBER} ."
+    sh "docker image build -t ${hubUser}/${project}:myrestapiapp-${env.BUILD_NUMBER} ."
     withCredentials([usernamePassword(
             credentialsId: "Docker",
             usernameVariable: "Username",
@@ -7,5 +7,5 @@ def call(String project, String hubUser) {
     )]) {
         sh "docker login -u '$Username' -p '$Password'"
     }
-    sh "docker image push ${hubUser}/${project}:beta-${env.BRANCH_NAME}-${env.BUILD_NUMBER}"
+    sh "docker image push ${hubUser}/${project}:myrestapiapp-${env.BUILD_NUMBER}"
 }

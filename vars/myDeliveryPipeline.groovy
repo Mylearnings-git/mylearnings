e31ff89a83df1a9abf95e2eceb0837a0dcc046b6
Mylearnings-git <62610302+Mylearnings-git@users.noreply.github.com>
2020-05-19 12:47:58 +0530

    Update myDeliveryPipeline.groovy

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -1,6 +1,9 @@
-def call(Map pipelineParams) {
+//def call(Map pipelineParams) {
+ def call(Map yuvaparam) { 
   
-  git branch: pipelineParams.branch, url: pipelineParams.scmUrl
+  //git branch: pipelineParams.branch, url: pipelineParams.scmUrl
+   
+   git branch: yuvaparam.branch, url: yuvaparam.scmUrl
 
   //git branch: ${branch}, url: ${url}
              

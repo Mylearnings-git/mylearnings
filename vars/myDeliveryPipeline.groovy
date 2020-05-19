c512e46c4a529f705e0c8eafdce8479f4d104c4b
Mylearnings-git <62610302+Mylearnings-git@users.noreply.github.com>
2020-05-19 09:12:05 +0530

    Update myDeliveryPipeline.groovy

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -1,20 +1,5 @@
 def call(Map pipelineParams) {
 
-    pipeline {
-        agent any
-        stages {
-            stage('checkout git') {
-                steps {
                     git branch: pipelineParams.branch, url: pipelineParams.scmUrl
-                }
-            }
-            stage('mvn build')
-                  {
-             steps
-                      {
-                          sh 'mvn clean install'
-                      }
-                  }
-}
-}
+             
 }

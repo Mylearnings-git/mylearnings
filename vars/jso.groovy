4d7aef0eb203900db50e623ff271b252f8508e7f
Mylearnings-git <62610302+Mylearnings-git@users.noreply.github.com>
2020-05-21 09:50:44 +0530

    Update jso.groovy

--- vars/jso.groovy
+++ vars/jso.groovy
@@ -1,6 +1,6 @@
+import groovy.json.JsonSlurper
 def call()
 {
-import groovy.json.JsonSlurper
 def filename = '/var/lib/jenkins/workspace/mysharedlib/Projects.json'
 def JsonSlurper = new JsonSlurper()
 data = JsonSlurper.parse(new File(filename))

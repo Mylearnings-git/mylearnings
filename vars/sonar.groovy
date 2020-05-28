7136b894ced05020273fec35fb48d09cf4f06b81
Mylearnings-git <62610302+Mylearnings-git@users.noreply.github.com>
2020-05-28 17:07:51 +0530

    Update sonar.groovy

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -1,7 +1,7 @@
 def call(String tool, String envname) {
-def scannerHome = tool 'tool';
-                          withSonarQubeEnv("envname") {
+  def scannerHome = tool '${tool}';
+  withSonarQubeEnv("${envname}") {
 
-                          sh "${tool("tool")}/bin/sonar-scanner -Dproject.settings=sonar-project.properties"
+                            sh "${tool("${tool}")}/bin/sonar-scanner -Dproject.settings=sonar-project.properties"
 }
 }

efb0561b9964195fb974ee950cd262893b9cee4f
Mylearnings-git <62610302+Mylearnings-git@users.noreply.github.com>
2020-05-29 12:06:46 +0530

    Create coverage.groovy

---
+++ vars/coverage.groovy
@@ -0,0 +1,11 @@
+def call(String class, String execPattern, String classPattern, String sourcePattern, String exclusionPattern) {
+step([$class: ${class},
+            execPattern: ${execPattern},
+           classPattern: ${classPattern},
+           sourcePattern}: ${sourcePattern},
+           exclusionPattern: ${exclusionPattern}
+           ])
+}           
+           
+           
+           

babbde1ff7a8108521b6e712beb670b0944289dd
Mylearnings-git <62610302+Mylearnings-git@users.noreply.github.com>
2020-05-29 12:27:37 +0530

    Update coverage.groovy

--- vars/coverage.groovy
+++ vars/coverage.groovy
@@ -1,11 +1,10 @@
 def call(String class, String execPattern, String classPattern, String sourcePattern, String exclusionPattern) {
 step([$class: ${class},
-            execPattern: ${execPattern},
+           execPattern: ${execPattern},
            classPattern: ${classPattern},
            sourcePattern: ${sourcePattern},
            exclusionPattern: ${exclusionPattern}
            ])
 }           
-           
-           
+                 
            

bd32b73a4ca6b957b77820df8ae28ddb66b2cb98
Mylearnings-git <62610302+Mylearnings-git@users.noreply.github.com>
2020-05-29 12:17:45 +0530

    Update kub.groovy

--- vars/kub.groovy
+++ vars/kub.groovy
@@ -1,14 +1,14 @@
- def call(String cluster, String region) {
-// withCredentials([[
- //$class: 'AmazonWebServicesCredentialsBinding', 
-// accessKeyVariable: 'AWS_ACCESS_KEY_ID', 
-// credentialsId: 'e261da0d-0895-4f7d-953f-28dbf1f27f1c', 
-// secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) 
-// {
+ def call(String cluster, String region, String kubecred) {
+withCredentials([[
+ $class: 'AmazonWebServicesCredentialsBinding', 
+ accessKeyVariable: 'AWS_ACCESS_KEY_ID', 
+ credentialsId: ${kubecred}, 
+ secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) 
+ {
               echo "Login Successfull"
               sh "aws eks --region '${region}'  update-kubeconfig --name '${cluster}'"
               sh 'sed -i s/"Build_Number"/"$BUILD_NUMBER"/g deployment.yml'
               sh 'kubectl apply -f deployment.yml'
               sh 'kubectl apply -f service.yml'
-          //    }
+             }
               }
